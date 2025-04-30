845c2260a6829813ac3b906caf07f0548ef83b82
Ilias BENHARRAT <ilias.benharrat@gmail.com>
2025-04-30 10:31:03 +0200

    Étape 6 : API fonctionnelle pour consulter et miner des blocs

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const Block = require('./block');
+const express = require('express');
+const Blockchain = require('./blockchain');
 
-const block = new Block(Date.now(), 'foo-lastHash', 'bar-hash', 'some data');
-console.log(block.toString());
-console.log('Block Hash:', Block.blockHash(block));
+const app = express();
+const blockchain = new Blockchain();
+
+app.use(express.json());
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const { data } = req.body;
+  const block = blockchain.addBlock(data);
+  res.json({ message: 'Block added!', block });
+});
+
+const PORT = process.env.HTTP_PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
