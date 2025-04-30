fd95f43e26468a6b8e31da6b17b07ef54e39cc33
Ilias BENHARRAT <ilias.benharrat@gmail.com>
2025-04-30 10:25:58 +0200

    Étape 5 : Validation et remplacement de chaîne + tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe("Blockchain", () => {
-  let blockchain;
+  let blockchain, blockchain2;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockchain2 = new Blockchain();
   });
 
   it("starts with genesis block", () => {
@@ -16,5 +17,33 @@ describe("Blockchain", () => {
     const data = 'foo';
     blockchain.addBlock(data);
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(data);
+  });
+
+  it("validates a valid chain", () => {
+    blockchain2.addBlock('foo');
+    expect(Blockchain.isValidChain(blockchain2.chain)).toBe(true);
+  });
+
+  it("invalidates a chain with a corrupt genesis block", () => {
+    blockchain2.chain[0].data = 'corrupt';
+    expect(Blockchain.isValidChain(blockchain2.chain)).toBe(false);
+  });
+
+  it("invalidates a corrupt chain", () => {
+    blockchain2.addBlock('foo');
+    blockchain2.chain[1].data = 'tampered';
+    expect(Blockchain.isValidChain(blockchain2.chain)).toBe(false);
+  });
+
+  it("replaces the chain with a valid longer chain", () => {
+    blockchain2.addBlock('foo');
+    blockchain.replaceChain(blockchain2.chain);
+    expect(blockchain.chain).toEqual(blockchain2.chain);
+  });
+
+  it("does not replace with a shorter or equal chain", () => {
+    blockchain.addBlock('foo');
+    blockchain.replaceChain(blockchain2.chain);
+    expect(blockchain.chain).not.toEqual(blockchain2.chain);
   });
 });
