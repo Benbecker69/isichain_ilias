5ffe2e3c477e7d6fc06e09e666232888144ba79e
Ilias BENHARRAT <ilias.benharrat@gmail.com>
2025-04-30 10:20:33 +0200

    Étape 3 : Ajout de mineBlock dans Block.js

--- block.js
+++ block.js
@@ -28,6 +28,14 @@ class Block {
     const { timestamp, lastHash, data } = block;
     return Block.hash(timestamp, lastHash, data);
   }
+
+  static mineBlock(lastBlock, data) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    const hash = Block.hash(timestamp, lastHash, data);
+
+    return new this(timestamp, lastHash, hash, data);
+  }
 }
 
 module.exports = Block;
